b0d75d46df40dfe1a7e5fbbb8c664f7cb9294dc1
ping <pingmeat73@gmail.com>
2023-09-16 20:12:49 +0530

    redirecting catalogue.sh script output to a file using variable

--- catalogue.sh
+++ catalogue.sh
@@ -1,30 +1,31 @@
+log=/tmp/roboshop.log
 echo -e "\e[36m>>>>>>>>>> copying service file <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 echo -e "\e[36m>>>>>>>>>> copying repo file <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 echo -e "\e[36m>>>>>>>>>> Install nodejs repo <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 echo -e "\e[36m>>>>>>>>>> install nodejs <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-yum install nodejs -y &>>/tmp/roboshop.log
+yum install nodejs -y &>>${log}
 echo -e "\e[36m>>>>>>>>>> creating application user <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-useradd roboshop &>>/tmp/roboshop.log
+useradd roboshop &>>${log}
 echo -e "\e[36m>>>>>>>>>> remove application dir <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-rm -rf /app &>>/tmp/roboshop.log
+rm -rf /app &>>${log}
 echo -e "\e[36m>>>>>>>>>> create application dir <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
+mkdir /app &>>${log}
 echo -e "\e[36m>>>>>>>>>> download application code <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
 echo -e "\e[36m>>>>>>>>>> extract application code <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip &>>${log}
 cd /app
  echo -e "\e[36m>>>>>>>>>> install nodejs dependencies <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-npm install &>>/tmp/roboshop.log
+npm install &>>${log}
  echo -e "\e[36m>>>>>>>>>> install mongodb client <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+yum install mongodb-org-shell -y &>>${log}
  echo -e "\e[36m>>>>>>>>>> Load catalogue schema <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-mongo --host mongodb.pradevops.online </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb.pradevops.online </app/schema/catalogue.js &>>${log}
  echo -e "\e[36m>>>>>>>>>> start catalogue service <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl daemon-reload &>>${log}
+systemctl enable catalogue &>>${log}
+systemctl restart catalogue &>>${log}
