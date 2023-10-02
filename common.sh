8e8b9019ba298ab1f0411ea0bd3c8c04b3b4bc48
ping <pingmeat73@gmail.com>
2023-10-02 19:54:52 +0530

    removing password from payment.service and payment.sh

--- common.sh
+++ common.sh
@@ -96,6 +96,7 @@ func_python() {
   yum install python36 gcc python3-devel -y &>>${log}
   func_exit_status
   func_apppreq
+  sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
   echo -e "\e[36m>>>>>>>>>> build ${component} service <<<<<<<<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
   func_exit_status
--- payment.sh
+++ payment.sh
@@ -1,5 +1,12 @@
 component=payment
+rabbitmq_app_password=$1
+if [ -z "${rabbitmq_app_password}" ]; then
+  echo Input rabbitmq AppUser Password Missing
+  exit 1
+fi
 source common.sh
 func_python
+
+
 
 
