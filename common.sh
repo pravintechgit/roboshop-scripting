fee9cfa360031e47e1f8740f8386c8faa9730ac2
ping <pingmeat73@gmail.com>
2023-10-02 17:23:45 +0530

    exit status in roboshop script

--- common.sh
+++ common.sh
@@ -1,25 +1,32 @@
 log=/tmp/roboshop.log
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+      echo -e "\e[32m SUCCESS \e[0m"
+      else
+        echo -e "\e[31m FAILURE \e[0m"
+    fi
+}
 func_apppreq() {
    echo -e "\e[36m>>>>>>>>>> copying ${component} service file <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
    cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-   echo $?
+   func_exit_status
     echo -e "\e[36m>>>>>>>>>> creating application user <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     useradd roboshop &>>${log}
-    echo $?
+    func_exit_status
     echo -e "\e[36m>>>>>>>>>> remove application dir <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     rm -rf /app &>>${log}
-    echo $?
+    func_exit_status
     echo -e "\e[36m>>>>>>>>>> create application dir <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     mkdir /app &>>${log}
-    echo $?
+    func_exit_status
     echo -e "\e[36m>>>>>>>>>> download application code <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-    echo $?
+   func_exit_status
     echo -e "\e[36m>>>>>>>>>> extract application code <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     cd /app
     unzip /tmp/${component}.zip &>>${log}
     cd /app
-    echo $?
+    func_exit_status
 }
 
 func_systemd() {
@@ -27,26 +34,26 @@ func_systemd() {
     systemctl daemon-reload &>>${log}
     systemctl enable ${component} &>>${log}
     systemctl restart ${component} &>>${log}
-    echo $?
+    func_exit_status
 }
 
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
      echo -e "\e[36m>>>>>>>>>> install mongodb client <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     yum install mongodb-org-shell -y &>>${log}
-    echo $?
+    func_exit_status
      echo -e "\e[36m>>>>>>>>>> Load ${component} schema <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     mongo --host mongodb.pradevops.online </app/schema/${component}.js &>>${log}
-    echo $?
+    func_exit_status
   fi
 
   if [ "${schema_type}" == "mysql" ]; then
       echo -e "\e[36m>>>>>>>>>> install mysql client <<<<<<<<<<<<<<<<<<\e[0m"
       yum install mysql -y &>>${log}
-      echo $?
+      func_exit_status
       echo -e "\e[36m>>>>>>>>>> Load ${component} schema <<<<<<<<<<<<<<<<<<\e[0m"
       mysql -h mysql.pradevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
-      echo $?
+      func_exit_status
   fi
 }
 
@@ -54,29 +61,29 @@ nodejs() {
   log=/tmp/roboshop.log
   echo -e "\e[36m>>>>>>>>>> copying mongodb repo file <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  echo $?
+  func_exit_status
   echo -e "\e[36m>>>>>>>>>> Install nodejs repo <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-  echo $?
+  func_exit_status
   echo -e "\e[36m>>>>>>>>>> install nodejs <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   yum install nodejs -y &>>${log}
-  echo $?
+  func_exit_status
 func_apppreq
    echo -e "\e[36m>>>>>>>>>> install nodejs dependencies <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   npm install &>>${log}
-  echo $?
+  func_exit_status
 func_schema_setup
 func_systemd
 }
 func_java() {
 echo -e "\e[36m>>>>>>>>>> Install Maven <<<<<<<<<<<<<<<<<<\e[0m"
 yum install maven -y &>>${log}
-echo $?
+func_exit_status
 func_apppreq
   echo -e "\e[36m>>>>>>>>>> build ${component} service <<<<<<<<<<<<<<<<<<\e[0m"
   mvn clean package &>>${log}
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
-  echo $?
+  func_exit_status
 func_schema_setup
  func_systemd
 }
@@ -84,10 +91,10 @@ func_schema_setup
 func_python() {
   echo -e "\e[36m>>>>>>>>>> Install python 3 <<<<<<<<<<<<<<<<<<\e[0m"
   yum install python36 gcc python3-devel -y &>>${log}
-  echo $?
+  func_exit_status
   func_apppreq
   echo -e "\e[36m>>>>>>>>>> build ${component} service <<<<<<<<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
-  echo $?
+  func_exit_status
  func_systemd
 }
