db2d671f2640bab00b8cbd2e70139f5491043260
ping <pingmeat73@gmail.com>
2023-10-02 16:50:31 +0530

    exit status in roboshop script

--- common.sh
+++ common.sh
@@ -2,18 +2,24 @@ log=/tmp/roboshop.log
 func_apppreq() {
    echo -e "\e[36m>>>>>>>>>> copying ${component} service file <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
    cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+   echo $?
     echo -e "\e[36m>>>>>>>>>> creating application user <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     useradd roboshop &>>${log}
+    echo $?
     echo -e "\e[36m>>>>>>>>>> remove application dir <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     rm -rf /app &>>${log}
+    echo $?
     echo -e "\e[36m>>>>>>>>>> create application dir <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     mkdir /app &>>${log}
+    echo $?
     echo -e "\e[36m>>>>>>>>>> download application code <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+    echo $?
     echo -e "\e[36m>>>>>>>>>> extract application code <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     cd /app
     unzip /tmp/${component}.zip &>>${log}
     cd /app
+    echo $?
 }
 
 func_systemd() {
@@ -21,21 +27,26 @@ func_systemd() {
     systemctl daemon-reload &>>${log}
     systemctl enable ${component} &>>${log}
     systemctl restart ${component} &>>${log}
+    echo $?
 }
 
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
      echo -e "\e[36m>>>>>>>>>> install mongodb client <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     yum install mongodb-org-shell -y &>>${log}
+    echo $?
      echo -e "\e[36m>>>>>>>>>> Load ${component} schema <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     mongo --host mongodb.pradevops.online </app/schema/${component}.js &>>${log}
+    echo $?
   fi
 
   if [ "${schema_type}" == "mysql" ]; then
       echo -e "\e[36m>>>>>>>>>> install mysql client <<<<<<<<<<<<<<<<<<\e[0m"
       yum install mysql -y &>>${log}
+      echo $?
       echo -e "\e[36m>>>>>>>>>> Load ${component} schema <<<<<<<<<<<<<<<<<<\e[0m"
       mysql -h mysql.pradevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+      echo $?
   fi
 }
 
@@ -43,23 +54,29 @@ nodejs() {
   log=/tmp/roboshop.log
   echo -e "\e[36m>>>>>>>>>> copying mongodb repo file <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  echo $?
   echo -e "\e[36m>>>>>>>>>> Install nodejs repo <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+  echo $?
   echo -e "\e[36m>>>>>>>>>> install nodejs <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   yum install nodejs -y &>>${log}
+  echo $?
 func_apppreq
    echo -e "\e[36m>>>>>>>>>> install nodejs dependencies <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   npm install &>>${log}
+  echo $?
 func_schema_setup
 func_systemd
 }
 func_java() {
 echo -e "\e[36m>>>>>>>>>> Install Maven <<<<<<<<<<<<<<<<<<\e[0m"
 yum install maven -y &>>${log}
+echo $?
 func_apppreq
   echo -e "\e[36m>>>>>>>>>> build ${component} service <<<<<<<<<<<<<<<<<<\e[0m"
   mvn clean package &>>${log}
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
+  echo $?
 func_schema_setup
  func_systemd
 }
@@ -67,8 +84,10 @@ func_schema_setup
 func_python() {
   echo -e "\e[36m>>>>>>>>>> Install python 3 <<<<<<<<<<<<<<<<<<\e[0m"
   yum install python36 gcc python3-devel -y &>>${log}
+  echo $?
   func_apppreq
   echo -e "\e[36m>>>>>>>>>> build ${component} service <<<<<<<<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
+  echo $?
  func_systemd
 }
